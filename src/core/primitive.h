bdf2b87f6c7e503b1c3c50d7505f6cbbc372b06c
Pavel Lokvenc <polygoncz@gmail.com>
2013-09-16 22:27:06 +0200

    Added Primitive class because future implement of acceleration structures.

--- src/core/primitive.h
+++ src/core/primitive.h
@@ -9,7 +9,20 @@
 
 using namespace std;
 
-class GeometricPrimitive : public ReferenceCounted
+class Primitive : public ReferenceCounted
+{
+public:
+	Primitive() {}
+	virtual ~Primitive() {}
+
+	virtual bool Intersect(const Ray& ray, float& tmin, Intersection& sr) = 0;
+	virtual bool IntersectP(const Ray& ray) = 0;
+	virtual bool CanIntersect() const = 0;
+	virtual void Refine(vector<Reference<GeometricPrimitive> > &refined) = 0;
+	virtual BBox Bounds() const = 0;
+};
+
+class GeometricPrimitive : public Primitive
 {
 public:
 	GeometricPrimitive(const Reference<Material>& _mat);
@@ -21,7 +34,6 @@ public:
 	virtual bool IntersectP(const Ray& ray);
 	virtual bool CanIntersect() const;
 	virtual void Refine(vector<Reference<GeometricPrimitive> > &refined);
-	virtual BBox Bounds() const = 0;
 
 	Material* GetMaterial(void) const;
 	void SetMaterial(const Reference<Material> &_material);
